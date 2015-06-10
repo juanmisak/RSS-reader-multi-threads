0cb0d200146192974e69a0cd5bc9b713aff540df
Sergio <sergio.chicho10@gmail.com>
2015-06-09 23:08:09 -0500

    creacion de vista y cambio de settings

--- RSS/web/views.py
+++ RSS/web/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+# Create your views here.
+import json
+from django.db import transaction
+from django.forms import model_to_dict
+from django.http import HttpResponse
+from django.shortcuts import render_to_response]
+
+def index(request):
+    data = {}
+
+    return render_to_response("home.html", data)
